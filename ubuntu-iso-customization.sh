4677bcd3784a86f7d8bdd645161db62017d56f96
leojimenezcr <leojimenezcr@gmail.com>
2017-09-08 11:57:54 -0600

    Ahora genera sumas de verificación

--- ubuntu-iso-customization.sh
+++ ubuntu-iso-customization.sh
@@ -103,3 +103,9 @@ sudo xorriso -as mkisofs -isohybrid-mbr isolinux/isohdpfx.bin \
 -c isolinux/boot.cat -b isolinux/isolinux.bin -no-emul-boot -boot-load-size 4 \
 -boot-info-table -eltorito-alt-boot -e boot/grub/efi.img -no-emul-boot \
 -isohybrid-gpt-basdat -o ../$CUSTOMISONAME .
+
+cd ..
+md5sum $CUSTOMISONAME >> MD5SUMS
+sha1sum $CUSTOMISONAME >> SHA1SUMS
+sha256sum $CUSTOMISONAME >> SHA256SUMS
+
